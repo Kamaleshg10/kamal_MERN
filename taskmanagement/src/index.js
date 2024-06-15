21438f4a4eea8f8b3dc3236bd5f6c2f90d917177
kamalesh <mechanical14358@gmail.com>
2024-06-15 20:12:27 +0530

    ready to deploy

--- taskmanagement/src/index.js
+++ taskmanagement/src/index.js
@@ -6,6 +6,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";  
 import { Provider } from 'react-redux';
 import { store } from './store';
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
+
+if (ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
